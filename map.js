2c5bf849b593766ece371f9993be72af15927a3c
boyang102 <66711671+boyang102@users.noreply.github.com>
2025-11-11 10:30:50 -0800

    step 2

--- map.js
+++ map.js
@@ -7,16 +7,54 @@ mapboxgl.accessToken =
 
 // Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', // ID of the div element
-  style: 'mapbox://styles/mapbox/streets-v12', // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
   minZoom: 5,
   maxZoom: 18,
 });
 
-// Optional: add navigation control (zoom buttons)
+// Add zoom controls
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
-// Confirm Mapbox loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+// Wait for map to fully load before adding layers
+map.on('load', async () => {
+  console.log('✅ Map fully loaded.');
+
+  // --- Boston Bike Lanes ---
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'boston-bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': '#32D400',
+      'line-width': 3,
+      'line-opacity': 0.5,
+    },
+  });
+
+  // --- Cambridge Bike Lanes ---
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://gis.cambridgema.gov/arcgis/rest/services/OpenData/OpenData_BikeFacilities/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=geojson',
+  });
+
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': '#0099FF',
+      'line-width': 3,
+      'line-opacity': 0.5,
+    },
+  });
+
+  console.log('🚴‍♀️ Boston and Cambridge bike lanes added!');
+});
